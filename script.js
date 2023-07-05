7362c5579a5f5092419916e4cd97367baf645ce2
Osamede <aghedopriscilla@gmail.com>
2023-07-05 20:06:18 +0100

    the fist page

---
+++ script.js
@@ -0,0 +1,12 @@
+// the cbt assignment 
+// for questions that you read through
+// for answers that you read through
+// for correct answer
+let home = document.getElementById("home")
+let cbt = document.getElementById("cbt")
+let time = document.getElementById
+cbt.style.display = "none"
+function myStart(){
+  home.style.display = "none"
+  cbt.style.display = "block"
+}
