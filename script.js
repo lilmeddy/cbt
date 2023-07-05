41ff4e065ecd1de09657058ec38c5353d55bf9e0
Osamede <aghedopriscilla@gmail.com>
2023-07-05 20:40:31 +0100

    done with the countdown

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
-// the cbt assignment 
+// the cbt assignment
 // for questions that you read through
 // for answers that you read through
 // for correct answer
-let home = document.getElementById("home")
-let cbt = document.getElementById("cbt")
-let time = document.getElementById
-cbt.style.display = "none"
-function myStart(){
-  home.style.display = "none"
-  cbt.style.display = "block"
+let home = document.getElementById("home");
+let cbt = document.getElementById("cbt");
+let time = document.getElementById("time");
+cbt.style.display = "none";
+function myStart() {
+  home.style.display = "none";
+  cbt.style.display = "block";
 }
+function count(min) {
+  let second = 60;
+  let interval = setInterval(() => {
+    time.innerHTML = min + ":" + (second < 10 ? "0" : "") + second;
+    if (second === 0) {
+      second = 60;
+      min--;
+      if (min < 0) {
+        clearInterval(interval);
+        cbt.style.display = "none";
+        home.style.display = "block";
+      }
+      if (min === 0 && second === 30) {
+        time.style.color = "red";
+      }
+    }
+    second--;
+  }, 1000);
+}
+count(1);
